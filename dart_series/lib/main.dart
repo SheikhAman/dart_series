c1e2b81fdf72b63b7e0186c7fe96d24c94457fdd
SheikhAman <amansk664@gmail.com>
2022-12-16 23:23:01 +0600

    print

--- dart_series/lib/main.dart
+++ dart_series/lib/main.dart
@@ -1,8 +1,54 @@
 void main() {
   // square shape
+  print('square shape');
   print('*******');
   print('*     *');
   print('*     *');
   print('*     *');
+  print('*     *');
   print('*******');
+
+  // rectangle shape
+  print('rectangle shape');
+  print('*****************');
+  print('*               *');
+  print('*               *');
+  print('*               *');
+  print('*****************');
+
+  // triangle shape
+  print('triangle shape');
+  print('        *       ');
+  print('      *****     ');
+  print('    *********   ');
+  print('  ************  ');
+  print('****************');
+
+  // pentagon shape
+  print('pentagon shape');
+  print('      *     ');
+  print('    *    *   ');
+  print('  *        *  ');
+  print(' *          * ');
+  print('  *        *  ');
+  print('    ******   ');
+
+  // circle shape
+  print('circle shape');
+  print('    ******    ');
+  print('*            *');
+  print('    ******    ');
+
+  // heart shape
+  print('heart shape');
+  print(' **   ** ');
+  print('**** ****');
+  print('  *****  ');
+  print('    *    ');
+
+  // pyramid shape
+  print('pyramid shape');
+  print('     *      ');
+  print('  ********');
+  print('*************');
 }
