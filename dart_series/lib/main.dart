0079d2c3a7bc78be44febbf8f2104edf3e4ab53b
SheikhAman <amansk664@gmail.com>
2023-02-01 12:06:35 +0600

    feet to meter, meter to km

--- dart_series/lib/main.dart
+++ dart_series/lib/main.dart
@@ -1,21 +1,12 @@
 void main() {
-// swap to variables value
-  int x;
-  x = 50;
-  int y;
-  y = 30;
+// convert feet to meter
+// convert meter tor km
 
-  x = 60;
+  double feet = 2000;
+  double meter, km;
 
-  print('before swaping');
-  print('x value is $x');
-  print('y value is $y');
-//code execute line by line
-  x = x + y; // 60+30 = 90
-  y = x - y; // 90 - 30 = 60
-  x = x - y; // 90 - 60 = 30
-
-  print('after swaping');
-  print('x value is $x');
-  print('y value is $y');
+  meter = feet / 3;
+  print('meter is $meter');
+  km = meter / 1000;
+  print('km:$km');
 }
